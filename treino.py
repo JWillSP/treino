5bc2f6f0a50b91e6954002b315bcb2c9f9c6be06
JWillSP <j12wil@gmail.com>
2020-05-14 12:04:05 -0300

    com lxml

--- treino.py
+++ treino.py
@@ -1,3 +1,5 @@
+import time
+from lxml import etree
 from selenium.webdriver import Firefox
 
 nav = Firefox()
@@ -6,11 +8,24 @@ nav.get('https://br.answers.yahoo.com')
 
 barra = nav.find_element_by_name("p")
 
+barra.send_keys('Python')
+
 submit = nav.find_element_by_xpath('//div[@id="searchbarContainer"]//button')
 
 submit.click()
 
+time.sleep(3)
+
 results = nav.find_elements_by_xpath('//div[@id="web"]/ol[2]//li')
 
+tree = etree.HTML(nav.page_source)
+
+results2 = tree.xpath('//div[@id="web"]/ol[2]//li')
+
 for resultado in results: print(resultado.text)
-#fazer com lxml também
+
+for r in results2:
+    for texto in r.itertext():
+        print(texto)
+
+nav.close()
